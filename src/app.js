7671c6adf00cd8735952a0173e7ba439e575f510
jovivaspo <jovivaspo@gmail.com>
2023-01-21 14:30:33 +0100

    Verification ssl

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const notFound = require("./middlewares/notFound");
 const handleError = require("./middlewares/handleError");
 const helmet = require("helmet");
+
+const nameFile = "DC2B0F6B913F64385D45F37DBA78B5FE.txt";
 
 const app = express();
 
@@ -13,6 +16,14 @@ app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
+
+//SSL
+app.get(
+  "/.well-known/pki-validation/DC2B0F6B913F64385D45F37DBA78B5FE.txt",
+  (req, res) => {
+    res.sendFile(path.resolve("./" + nameFile));
+  }
+);
 
 //Routes
 app.use("/api/users", require("./routes/users"));
