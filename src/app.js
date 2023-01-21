79100f4199852da575083fea70c1f85dcf427f8f
jovivaspo <jovivaspo@gmail.com>
2023-01-21 20:05:32 +0100

    SSL

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
 const notFound = require("./middlewares/notFound");
 const handleError = require("./middlewares/handleError");
@@ -7,8 +6,6 @@ const helmet = require("helmet");
 const createAdmin = require("./services/createAdmin");
 
 const app = express();
-
-const nameFile = "03AE6B7A9F3CD4C727A91AF0D9EAC481.txt";
 
 /*Verify or created admin*/
 createAdmin();
@@ -22,10 +19,10 @@ app.use(express.json());
 app.use(helmet());
 
 //SSL VERIFCATION
-//SSL
+/*
 app.get("/.well-known/pki-validation/" + nameFile, (req, res) => {
   res.sendFile(path.resolve("./" + nameFile));
-});
+});*/
 
 //Routes
 app.use("/api/users", require("./routes/users"));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
- require('dotenv').config()
-const app = require('./app')
-require('./database')
+require("dotenv").config();
+const app = require("./app");
+require("./database");
+const fs = require("fs");
+const https = require("https");
 
-app.listen(app.get('port'),()=>{
-    console.log('Server on port: ', app.get('port'))
-})
+const key = fs.readFileSync("private.key");
+const cert = fs.readFileSync("certificate.crt");
+
+const cred = {
+  key,
+  cert,
+};
+
+app.listen(app.get("port"), () => {
+  console.log("Server on port: ", app.get("port"));
+});
+
+const httpsServer = https.createServer(cred, app);
+
+httpsServer.listen(8443);
